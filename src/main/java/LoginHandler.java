b90a4e2066a31935944571a98daadbefd52a727a
etriest <emil.triest@telekom.de>
2022-07-04 14:45:29 +0200

    Initial commit

---
+++ src/main/java/LoginHandler.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginHandler")
+public class LoginHandler extends HttpServlet {
+
+    private final String username = "admin";
+    private final String password = "password";
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        // get request parameters for username and password
+        String username = request.getParameter("username");
+        String password = request.getParameter("pwd");
+
+        if (this.username.equals(username) && this.password.equals(password)) {
+            //get the old session and invalidate
+            HttpSession oldSession = request.getSession(false);
+            if (oldSession != null) {
+                oldSession.invalidate();
+            }
+            //generate a new session
+            HttpSession newSession = request.getSession(true);
+
+            //setting session to expiry in 5 mins
+            newSession.setMaxInactiveInterval(5*60);
+
+            System.out.println("=====================" + newSession.getAttributeNames());
+
+            response.setHeader("Set-Cookie", ";HttpOnly; SameSite=None");
+            response.sendRedirect("admin/LoginSuccess.jsp");
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/loginPage.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either username or password is wrong.</font>");
+            rd.include(request, response);
+        }
+    }
+}
